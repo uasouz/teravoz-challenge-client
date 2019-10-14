cd1698b88666c3cf99187ed003678c0847e7bde8
Hadara <vlopes45@gmail.com>
2019-10-13 23:22:19 -0400

    Minor adjusments to presentation

--- src/components/CallList/sub-components/CallItem/CallItem.tsx
+++ src/components/CallList/sub-components/CallItem/CallItem.tsx
@@ -24,6 +24,7 @@ const useStyles = makeStyles(theme => ({
     paper: {
         padding: theme.spacing(2),
         textAlign: 'left',
+        marginTop: 10,
         color: theme.palette.text.secondary,
     },
     modalPaper: {
--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -2,12 +2,13 @@ import React from "react"
 import "./MainContent.css"
 import CallList from "../CallList";
 import store from "../../modules/store";
-import {Grid} from "@material-ui/core";
+import {Grid, Typography} from "@material-ui/core";
 
 const MainContent = () => {
     console.log("State", store.getState());
     return (
         <main className="Main">
+            <Typography variant={"h4"}>Click on a call to see its Events</Typography>
             <Grid className={"dashboard"}
                   spacing={2}
                   container
